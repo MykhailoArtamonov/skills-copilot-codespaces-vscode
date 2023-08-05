1fd35e524423524a326d6673c451028581c092cb
MykhailoArtamonov <100056522+MykhailoArtamonov@users.noreply.github.com>
2023-08-05 21:02:52 +0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMemberTextValue = skillsMemberText + " " + skillsMemberValue;
+    var skillsMemberTextValueSplit = skillsMemberTextValue.split(" ");
+    var skillsMemberTextValueSplitLength = skillsMemberTextValueSplit.length;
+    var skillsMemberTextValueSplitLengthMinus = skillsMemberTextValueSplitLength - 1;
+    var skillsMemberTextValueSplitLengthMinusValue = skillsMemberTextValueSplit[skillsMemberTextValueSplitLengthMinus];
+    var skillsMemberTextValueSplitLengthMinusValueSplit = skillsMemberTextValueSplitLengthMinusValue.split("-");
+    var skillsMemberTextValueSplitLengthMinusValueSplitLength = skillsMemberTextValueSplitLengthMinusValueSplit.length;
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinus = skillsMemberTextValueSplitLengthMinusValueSplitLength - 1;
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValue = skillsMemberTextValueSplitLengthMinusValueSplit[skillsMemberTextValueSplitLengthMinusValueSplitLengthMinus];
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplit = skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValue.split("=");
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLength = skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplit.length;
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinus = skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLength - 1;
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinusValue = skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplit[skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinus];
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinusValueSplit = skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinusValue.split(">");
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinusValueSplitLength = skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinusValueSplit.length;
+    var skillsMemberTextValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinusValueSplitLengthMinus
